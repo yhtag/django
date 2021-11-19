4f7f00a3e1c0eaf2e70e0a14c3e974035997998e
YHtAg <yehtetag.y@gmail.com>
2021-11-19 21:09:14 +0630

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views    
+
+urlpatterns = [
+    path('', views.dashboard),
+    path('customers/<int:id>', views.customers),
+    path('products/', views.products),
+]
---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from accounts.models import *
+
+def customers(request,id):
+    customer=Customer.objects.get(id=id)
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    return render(request,'accounts/customers.html',{
+        'customer':customer,
+        'orders':orders,
+        'order_count':order_count
+    })
+
+def products(request):
+    products=Product.objects.all()
+    return render(request,'accounts/products.html',{
+        'products' : products
+    })
+
+def dashboard(request):
+    customers=Customer.objects.all()
+    orders=Order.objects.all()
+    total=orders.count();
+    delivered=Order.objects.filter(status='delivered').count()
+    pending=Order.objects.filter(status='pending').count()
+    return render(request,'accounts/dashboard.html',{
+        'customers' : customers,
+        'orders' : orders,
+        'total' : total,
+        'delivered' : delivered,
+        'pending' : pending
+    })
